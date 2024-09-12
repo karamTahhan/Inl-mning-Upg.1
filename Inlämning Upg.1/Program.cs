8d458a15fbfcdaf901e0443a01e95cb577ff8a97
karamTahhan <kata07001@utb.vaxjo.se>
2024-09-12 14:11:17 +0200

    Add project files.

---
+++ Inlämning Upg.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; 
+
+namespace Uppgift_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("du är nästan klar med kap1");
+            Console.WriteLine("du har lärt dig göra tecken som \" och \\");
+
+        }
+
+
+    }
+
+
+
+
+}
+
